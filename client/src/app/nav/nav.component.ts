d51d473881c132eeae1d4072bb09ca6a200d8eb5
callumlukedavies <callumlukedavies01@gmail.com>
2022-08-22 13:47:59 +0100

    End of Section 5

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  model: any = {}
+
+  constructor(public accountService: AccountService) { }
+
+  //lifecyce hook which is called when the directive is initialised
+  ngOnInit(): void {
+  }
+
+  login(){
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  logout(){
+    this.accountService.logout();
+  }
+
+}
